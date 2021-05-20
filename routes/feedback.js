21e89529da2dd8ec215e349e2c3d32fa1cc7c0d6
mduduziavatar <mduduziavatar@github.com>
2021-05-20 09:05:30 +0200

    added api to feedback form

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,6 +4,30 @@ let { check, validationResult } = require('express-validator');
 
 let router = express.Router();
 
+let validations = [
+  check('name') //check the name
+    .trim() //remove empty characters
+    .isLength({ min: 3 }) //ensure the length is at least 3 characters long
+    .escape() //no html and js embedded in this entry
+    .withMessage('A name is required'),
+  check('email') //check the name
+    .trim() //remove empty characters
+    .isEmail() //ensure that item is email address
+    .normalizeEmail() //make email lowercase
+    .escape() //no html and js embedded in this entry
+    .withMessage('Email address is required'),
+  check('title') //check the name
+    .trim() //remove empty characters
+    .isLength({ min: 2 }) //ensure the length is at least 2 characters long
+    .escape() //no html and js embedded in this entry
+    .withMessage('A title is required'),
+  check('message') //check the name
+    .trim() //remove empty characters
+    .isLength({ min: 10 }) //ensure the length is at least 15 characters long
+    .escape() //no html and js embedded in this entry
+    .withMessage('A message is required'),
+];
+
 module.exports = function (params) {
   let { feedbackService } = params;
 
@@ -12,48 +36,24 @@ module.exports = function (params) {
       let feedback = await feedbackService.getList();
 
       let errors = request.session.feedback ? request.session.feedback.errors : false;
-      
+
       let successMessage = request.session.feedback ? request.session.feedback.message : false;
-      
+
       request.session.feedback = {}; //request the session object
       return response.render('layout', {
         pageTitle: 'Feedback',
         template: 'feedback',
         feedback,
         errors,
-        successMessage
+        successMessage,
       });
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post(
-    '/',
-    [
-      check('name') //check the name
-        .trim() //remove empty characters
-        .isLength({ min: 3 }) //ensure the length is at least 3 characters long
-        .escape() //no html and js embedded in this entry
-        .withMessage('A name is required'),
-      check('email') //check the name
-        .trim() //remove empty characters
-        .isEmail() //ensure that item is email address
-        .normalizeEmail() //make email lowercase
-        .escape() //no html and js embedded in this entry
-        .withMessage('Email address is required'),
-      check('title') //check the name
-        .trim() //remove empty characters
-        .isLength({ min: 2 }) //ensure the length is at least 2 characters long
-        .escape() //no html and js embedded in this entry
-        .withMessage('A title is required'),
-      check('message') //check the name
-        .trim() //remove empty characters
-        .isLength({ min: 10 }) //ensure the length is at least 15 characters long
-        .escape() //no html and js embedded in this entry
-        .withMessage('A message is required'),
-    ],
-    async function (request, response) {
+  router.post('/', validations, async function (request, response, next) {
+    try {
       let errors = validationResult(request);
 
       if (!errors.isEmpty()) {
@@ -65,11 +65,28 @@ module.exports = function (params) {
 
       await feedbackService.addEntry(name, email, title, message);
       request.session.feedback = {
-        message: "Thank you for your message!"
-      }
+        message: 'Thank you for your message!',
+      };
       return response.redirect('/feedback');
+    } catch (err) {
+      return next(err);
     }
-  );
+  });
+
+  router.post('/api', validations, async function (request, response, next) {
+    try {
+      let errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.json({ errors: errors.array() });
+      }
+      let { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      let feedback = await feedbackService.getList();
+      return response.json({feedback, successMessage: 'Thank you for your feedback!'});
+    } catch (err) {
+      return next(err)
+    }
+  });
 
   return router;
 };
